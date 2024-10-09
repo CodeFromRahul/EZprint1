1f4c3ef3aba86098ab2c735f64543ec216475516
CodeFromRahul <rahulvish194002@gmail.com>
2024-10-10 02:22:39 +0530

    DOne with controllers

--- backend/src/controllers/fileSharing.controller.js
+++ backend/src/controllers/fileSharing.controller.js
@@ -1,28 +1,63 @@
-import { v4 as uuidv4 } from 'uuid'; // Import uuid to generate unique IDs
-import Session from '../models/sessionModel.js'; // Import your Session model
-import { generateQRCode } from '../utils/qrCodeGenerator.utils.js'; // Assuming you have a utility to generate QR codes
+import { v4 as uuidv4 } from 'uuid'; 
+import Session from '../models/sessionModel.js'; 
+import { generateQRCode } from '../utils/qrCodeGenerator.utils.js'; 
+import path from 'path';
+
 
 export const generateQRCodeController = async (req, res) => {
   try {
-    // Automatically generate sessionId and deviceId using uuidv4
+   
     const sessionId = uuidv4();
     const deviceId = uuidv4();
 
-    // Create a new session and save it to MongoDB
+  
     const session = new Session({ sessionId, deviceId });
     await session.save();
 
-    // Generate QR data containing sessionId
     const qrData = `http://localhost:5000/api/session/receive/${sessionId}`;
     
-    // Generate the QR code image based on the qrData
+  
     const qrCodeImage = await generateQRCode(qrData);
 
-    // Send the QR code as a PNG image in the response
+  
     res.type('png').send(qrCodeImage);
   } catch (error) {
     res.status(500).json({ message: 'Error generating QR Code', error });
   }
 };
+
+
+
+export const recieveFiles = async(req,res)=>{
+  const {sessionId}= req.param;
+
+  const session = Session.findOne({sessionId})
+  if(!session) return res.status(404).json({message:"File does not sed"})
+
+    res.status(200).json({
+      message: 'Files uploaded successfully',
+      files: files.map((file) => ({
+        fileName: file.originalname,
+        filePath: path.resolve(file.path),
+      })),
+    });
+
+}
+
+
+export const downloadFiles = async (req, res) => {
+  const { sessionId } = req.params;
+
+  const session = await Session.findOne({ sessionId });
+  if (!session) return res.status(404).json({ message: 'Invalid session' });
+
+ 
+  res.status(200).json({ message: 'Ready to download files' });
+};
+
+
+
+
+
 
 
